254a04728b56b1e828048db02867d24aef7dc950
jca26c <78522594+jca26c@users.noreply.github.com>
2021-09-29 00:37:16 -0500

    Update urls.py

--- spell_check/urls.py
+++ spell_check/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from main import views
 from django.urls import path, include
 
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	path('', include('main.urls')),
+    path('ajax-test-view/', views.index, name='ajax-test-view'),
+	path('', include('main.urls'))
 ]
